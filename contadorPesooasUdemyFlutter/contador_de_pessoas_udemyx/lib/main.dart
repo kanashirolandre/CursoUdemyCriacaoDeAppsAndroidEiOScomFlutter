c7448ed32cf2aeaf77057a1c0d640919542bb2b1
André Luiz H. Kanashiro <alhkanashiro@gmail.com>
2019-10-09 11:31:20 -0300

    Novos Itens Adicionados parte 2

--- contadorPesooasUdemyFlutter/contador_de_pessoas_udemyx/lib/main.dart
+++ contadorPesooasUdemyFlutter/contador_de_pessoas_udemyx/lib/main.dart
@@ -9,13 +9,24 @@ void main() {
       Text("Pessoas: 0", 
       style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
       ),        
-       FlatButton(
+      Row(
+        children: <Widget>[
+          FlatButton(
          child: Text(
            "+1",
            style: TextStyle(fontSize: 40.0, color: Colors.white),
            ),
            onPressed: () {},
            ),
+           FlatButton(
+         child: Text(
+           "-1",
+           style: TextStyle(fontSize: 40.0, color: Colors.white),
+           ),
+           onPressed: () {},
+           ),
+        ],
+      ),
        Text(
          "Pode Entrar!", 
       style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic,
